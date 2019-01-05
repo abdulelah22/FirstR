20ecaacfb3e2b26ddbdf8a852e6ccf14f4203084
abdulelah22 <34347940+abdulelah22@users.noreply.github.com>
2019-01-05 18:34:03 +0300

    Box Area Calculation.

---
+++ Abdulelah.java
@@ -0,0 +1,16 @@
+package Package2;
+
+public class Box {
+    public double length; //Class level Data.
+    public double width;
+
+    public double calculateArea(int length,int width)
+
+    // Passing arguments.
+    {
+        double area = this.length * this.width;
+
+        System.out.println("Area is " + area );
+        return area;
+    }
+}
